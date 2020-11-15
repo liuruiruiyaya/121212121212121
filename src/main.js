830bc214c1f4b7327af0533a8e689e46020b4926
liuhanhanya <7802578+liuhanhanya@user.noreply.gitee.com>
2020-11-15 23:15:51 +0800

    121

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import url from './router/url'
+import echarts from 'echarts'
+import "./router/route"
 Vue.config.productionTip = false
+import VueCookies from 'vue-cookies'
+Vue.use(VueCookies)
 
+Vue.prototype.$url = url
+Vue.prototype.$echarts = echarts
 new Vue({
+  url,
   router,
   store,
   render: h => h(App)
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'left',
+    component: () => import('../views/letf.vue'),
+    children:[
+      {
+        path:'mine',
+        component: () => import('../views/mine.vue')
+      },
+      {
+        path:'list',
+        component: () => import('../views/list.vue')
+      },
+      {
+        path:'kf',
+        component: () => import('../views/kf.vue')
+      },
+      {
+        path:'content',
+        component: () => import('../views/content.vue')
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:'/main',
+    component: () => import('../views/main.vue')
+  },
+  {
+    path:'/logon',
+    component: () => import('../views/header.vue')
   }
 ]
 
